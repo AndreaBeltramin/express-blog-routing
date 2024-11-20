2de4178d9283966e5de9e0508f7e858b58102b17
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-20 16:07:07 +0100

    creazione rotte per le operazioni crud

---
+++ db/blogPosts.js
@@ -0,0 +1,36 @@
+//creo un array dove inserisco una lista di almeno 5 post, per ognuno indico titolo,
+//contenuto, immagine e tags sotto forma di array di stringhe
+const blogPosts = [
+	{
+		title: "ciambellone",
+		content: "ciambellone fatto in casa",
+		img: "./images/ciambellone.jpeg",
+		tags: ["cucina", "dolce", "dessert"],
+	},
+	{
+		title: "cracker di barbabietola",
+		content: "cracker di barbabietola fatti in casa",
+		img: "./images/cracker_barbabietola.jpeg",
+		tags: ["cucina", "antipasto"],
+	},
+	{
+		title: "pane dolce fritto",
+		content: "pane dolce fritto fatto in casa",
+		img: "./images/pane_fritto_dolce.jpeg",
+		tags: ["cucina", "panificazione", "dolce"],
+	},
+	{
+		title: "pasta alla barbabietola",
+		content: "pasta alla barbabietola fatta in casa",
+		img: "./images/pasta_barbabietola.jpeg",
+		tags: ["cucina", "pasta", "primo"],
+	},
+	{
+		title: "torta paesana",
+		content: "torta paesana fatta in casa",
+		img: "./images/torta_paesana.jpeg",
+		tags: ["cucina", "dolce", "dessert"],
+	},
+];
+
+module.exports = blogPosts;
---
+++ routers/posts.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const blogPosts = require("../db/blogPosts.js");
+
+// rotta index
+// creo una rotta /bacheca che restituisce un oggetto json con la lista dei post e il conteggio partendo da un array
+router.get("/", (req, res) => {
+	res.json([blogPosts, "il numero di post è: " + blogPosts.length]);
+});
+
+// rotta show
+router.get("/:id", (req, res) => {
+	const id = req.params.id;
+	res.json(`Mostra il post con id n. ${id}`);
+});
+
+// rotta store
+router.post("/", (req, res) => {
+	res.json("Crea un nuovo post");
+});
+
+// rotta update
+router.put("/:id", (req, res) => {
+	const id = req.params.id;
+	res.json(`Modifica totale del post con id n. ${id}`);
+});
+
+// rotta modify
+router.patch("/:id", (req, res) => {
+	const id = req.params.id;
+	res.json(`Modifca parziale del post con id n. ${id}`);
+});
+
+// rotta destroy
+router.delete("/:id", (req, res) => {
+	const id = req.params.id;
+	res.json(`Elimina il post con id n. ${id}`);
+});
+
+module.exports = router;
